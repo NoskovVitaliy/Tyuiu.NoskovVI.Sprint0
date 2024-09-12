cb2b12883fe5320e92eb1b8418828ae855959b9d
NoskovVitaliy <Tyuiu.NoskovVI@outlook.com>
2024-09-13 00:19:13 +0500

    Добавил Task1

---
+++ Tyuiu.NoskovVI.Sprint0.Task1.V0/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tyuiu.NoskovVi.Sprint0.Task1.V0
+{
+
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Hello, world");
+            string name = "Виталий";
+            int age = 18;
+            Console.WriteLine("Здравствуйте, меня зовут " + name + " мне " + age + " лет!");
+        }
+    }
+}
